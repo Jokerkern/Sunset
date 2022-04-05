31e9041ccfa785c684ad1a191d91caee51d34b8b
Kern <852946650@qq.com>
2022-04-05 21:47:49 +0800

    second commit

---
+++ build/CMakeFiles/sunnet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sunnet.dir/src/main.cpp.o"
+  "sunnet"
+  "sunnet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sunnet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
