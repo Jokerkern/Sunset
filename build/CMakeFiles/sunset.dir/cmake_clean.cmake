4209ad8a080175dd133584d36d8363fc605e95bc
Kern <852946650@qq.com>
2022-05-02 01:23:04 +0800

    remove sunnet

---
+++ build/CMakeFiles/sunset.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sunset.dir/src/main.cpp.o"
+  "sunset"
+  "sunset.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sunset.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
